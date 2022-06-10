3c00da3b4275ade893839a65ec41d1cfb1f8239f
ray-blue <98744111+ray-blue@users.noreply.github.com>
2022-06-10 21:14:24 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     testnet: {
       url: 'https://polygon-mumbai.g.alchemy.com/v2/9ue0qc1c1dcfVpEIdLDYIivPP0BN9eGK',
-      accounts: ["5ea08d22cea4c7e4643653e617b2f56c2e3751e7717ccaee2ea9ded98268e79e"],
+      accounts: [privateKey],
     },
     mainnet: {
       url: 'https://polygon-rpc.com',
